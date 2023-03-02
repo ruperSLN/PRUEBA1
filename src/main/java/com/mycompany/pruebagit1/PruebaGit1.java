b45b3359afd44d6bdcdaa1913a100a0aa427cdf4
ruperSLN <ruper1903@gmail.com>
2023-03-02 20:35:03 +0100

    commit prueba git 2

--- src/main/java/com/mycompany/pruebagit1/PruebaGit1.java
+++ src/main/java/com/mycompany/pruebagit1/PruebaGit1.java
@@ -11,6 +11,6 @@ package com.mycompany.pruebagit1;
 public class PruebaGit1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello Manolo como estas!");
+        System.out.println("Hello Manolo como estas 2!");
     }
 }
